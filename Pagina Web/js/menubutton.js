0666639fa15ce70edee55561c8684881fbb14295
Lautaro Morales <lautamorales17@gmail.com>
2021-10-25 00:39:43 -0300

    Correcion: Se elimino una linea de codigo sin usar en el archivo javascript.

--- Pagina Web/js/menubutton.js
+++ Pagina Web/js/menubutton.js
@@ -8,7 +8,6 @@ function enviarSugerencia(e) {
     e.preventDefault();
     let form = new FormData(formulario);
     let nombre = form.get("nombre");
-    let sugerencia = form.get("sugerencia");
     let mensaje = document.querySelector("#mensajeForm");
     mensaje.innerHTML = `Hola, ${nombre}. Su sugerencia ha sido recibida y sera evaluada.`;
 }
