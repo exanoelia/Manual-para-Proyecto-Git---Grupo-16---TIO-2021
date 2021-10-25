a3c4c9ec5c523e1ad46649ddeccff77c093d8efc
Lautaro Morales <lautamorales17@gmail.com>
2021-10-25 00:38:28 -0300

    Agregue un span al index.html, agregue mensaje al enviar el form del html.

--- Pagina Web/js/menubutton.js
+++ Pagina Web/js/menubutton.js
@@ -1,3 +1,14 @@
-use "strict";
+"use strict";
 
 console.log("Test");
+let formulario = document.querySelector("#formSugerencias");
+formulario.addEventListener("submit",enviarSugerencia);
+
+function enviarSugerencia(e) {
+    e.preventDefault();
+    let form = new FormData(formulario);
+    let nombre = form.get("nombre");
+    let sugerencia = form.get("sugerencia");
+    let mensaje = document.querySelector("#mensajeForm");
+    mensaje.innerHTML = `Hola, ${nombre}. Su sugerencia ha sido recibida y sera evaluada.`;
+}
